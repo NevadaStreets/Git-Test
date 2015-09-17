432fec7a1e507fed09407ef86cf969c601c25f13
NevadaStreets <vidominguez@uc.cl>
2015-09-17 09:08:04 -0300

    Funcion suma agregada

--- Program01/src/Main.java
+++ Program01/src/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 
 
 	}
-
+	
+	static int suma(int a, int b){
+		int c = a + b;
+		return c;
+	}
 }
